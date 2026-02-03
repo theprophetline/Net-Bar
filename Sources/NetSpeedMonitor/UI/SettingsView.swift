49c2ab2bafa40a878b4de8f8a30f23a6b21d240f
Aditya <adityadav9532@gmail.com>
2026-02-04 00:03:59 +0530

    v1.4

--- Sources/NetSpeedMonitor/UI/SettingsView.swift
+++ Sources/NetSpeedMonitor/UI/SettingsView.swift
@@ -46,7 +46,7 @@ struct SettingsView: View {
                         Text("Net Bar")
                             .font(.title3)
                             .fontWeight(.semibold)
-                        Text("Version 1.3")
+                        Text("Version 1.4")
                             .font(.caption)
                             .foregroundStyle(.secondary)
                     }
